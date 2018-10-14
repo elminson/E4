0806c534d66a4565c90f9cc53149d253455a9006
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-10-14 19:56:34 -0400

    Update Router
    new post controller test

--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@
  * PHP version 5.4
  */
 
+// Require the controller class
+require '../App/Controllers/Posts.php';
+
 /**
  * Routing
  */
@@ -15,24 +18,7 @@ $router = new Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 
-// Display the routing table
-echo '<pre>';
-//var_dump($router->getRoutes());
-echo htmlspecialchars(print_r($router->getRoutes(), true));
-echo '</pre>';
-
-
-// Match the requested route
-$url = $_SERVER['QUERY_STRING'];
-
-if ($router->match($url)) {
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '</pre>';
-} else {
-    echo "No route found for URL '$url'";
-}
+$router->dispatch($_SERVER['QUERY_STRING']);
