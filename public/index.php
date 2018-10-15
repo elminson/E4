9561d8a864a5483833a016162762825983ffc234
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-10-14 23:43:41 -0400

    Adding Composer

--- public/index.php
+++ public/index.php
@@ -7,20 +7,9 @@
  */
 
 /**
- * Twig
+ * Composer
  */
-require_once dirname(__DIR__) . '/vendor/Twig/lib/Twig/Autoloader.php';
-
-/**
- * Autoloader
- */
-spl_autoload_register(function ($class) {
-    $root = dirname(__DIR__);   // get the parent directory
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if (is_readable($file)) {
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
+require '../vendor/autoload.php';
 
 
 /**
