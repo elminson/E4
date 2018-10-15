8ff0c1e5efaee4e79b7a34fb50fd92c8e0e6287d
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-10-14 22:05:33 -0400

    Adding Autoloader

--- public/index.php
+++ public/index.php
@@ -6,19 +6,27 @@
  * PHP version 5.4
  */
 
-// Require the controller class
-require '../App/Controllers/Posts.php';
+/**
+ * Autoloader
+ */
+spl_autoload_register(function ($class) {
+    $root = dirname(__DIR__);   // get the parent directory
+    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+    if (is_readable($file)) {
+        require $root . '/' . str_replace('\\', '/', $class) . '.php';
+    }
+});
+
 
 /**
  * Routing
  */
-require '../Core/Router.php';
 
-$router = new Router();
+$router = new Core\Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
-
+    
 $router->dispatch($_SERVER['QUERY_STRING']);
